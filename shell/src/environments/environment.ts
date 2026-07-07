dc9b079c1f12b5bb40baf09233b7c7aced432841
Suresh kanzariya <suresh.kanzariya@creditt.in>
2026-07-07 18:02:49 +0530

    Use environment API URL

--- shell/src/environments/environment.ts
+++ shell/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'http://localhost:5000/api',
 
   remotes: {
     product: 'http://localhost:4201/remoteEntry.js',
