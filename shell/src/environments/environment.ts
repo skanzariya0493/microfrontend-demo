50a1157d6c2a411865a8225b74ff29f19564b2e7
Suresh kanzariya <suresh.kanzariya@creditt.in>
2026-07-07 15:38:56 +0530

    Changes env files

---
+++ shell/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000/api',
+
+  remotes: {
+    product: 'http://localhost:4201/remoteEntry.js',
+    order: 'http://localhost:4202/remoteEntry.js'
+  }
+};
